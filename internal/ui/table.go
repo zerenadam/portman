03395f5a2994b7a264e0b1756dd38c2076c0441a
zadam <zerenadam7@gmail.com>
2026-04-20 18:29:30 -0400

    add: kill func

--- internal/ui/table.go
+++ internal/ui/table.go
@@ -1,8 +1,13 @@
 package ui
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+	"github.com/zerenadam/portman/internal/harbor"
 )
 
 func (a *App) initTable() {
@@ -16,7 +21,8 @@ func (a *App) initTable() {
 		switch event.Rune() {
 		case 'q':
 			a.app.Stop()
-			
+		case 'd':
+			a.killSelected()
 		}
 		return event 
 	})
@@ -27,7 +33,7 @@ func (a *App) initTable() {
 func (a *App) populateTable() {
 	a.table.Clear()
 	
-	headers := []string{"process", "PID", "@", "protocol", "user"}
+	headers := []string{"process", "PID", "address", "protocol", "user"}
 	for col, h := range headers {
 		a.table.SetCell(0, col, tview.NewTableCell(h).
 		SetExpansion(1).
@@ -43,3 +49,27 @@ func (a *App) populateTable() {
 		a.table.SetCell(r, 4, tview.NewTableCell(p.User).SetExpansion(1))
 	}
 }
+
+func (a *App) killSelected() {
+    row, _ := a.table.GetSelection()
+    if row == 0 { return }  // don't kill on header row
+
+    pidStr := a.table.GetCell(row, 1).Text
+    pid, err := strconv.Atoi(pidStr)
+    if err != nil { return }
+
+    proc, err := os.FindProcess(pid)
+    if err != nil {
+        // show error somewhere
+        return
+    }
+
+    if err := proc.Signal(os.Interrupt); err != nil {
+        fmt.Println("kill failed:", err)
+    }
+
+    // refresh after kill
+    a.ports = harbor.FindPorts()
+    a.populateTable()
+}
+
